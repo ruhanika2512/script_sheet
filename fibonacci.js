c96dd412c925ff1528d08b051bb760875783043f
Ruhanika shah <shahruhanika708@gmail.com>
2025-11-01 20:15:46 +0530

    Add files via upload

---
+++ fibonacci.js
@@ -0,0 +1,10 @@
+
+function fibonacciSeries(n) {
+    let fib = [0, 1];
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib.slice(0, n);
+}
+
+console.log(fibonacciSeries(10));
