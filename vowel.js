e176a4128fb53f2fc13a25e3c96f33365e1783cb
Ruhanika shah <shahruhanika708@gmail.com>
2025-11-01 19:55:38 +0530

    Add files via upload

---
+++ vowel.js
@@ -0,0 +1,8 @@
+let letter = "a";
+if (letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u") {
+    console.log("vowel");
+}
+else {
+    console.log("consonant");
+}
+
